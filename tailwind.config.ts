22d7d44cbe8dde6539986895fc24c0466e3a3824
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-02-18 01:54:41 +0400

    feat: add buttons for creating new fields

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,15 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "2rem",
+        sm: "2rem",
+        md: "5rem",
+        lg: "10rem",
+        xl: "20rem",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
