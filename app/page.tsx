44e0acf3e57db22c67f82a750c0063f88d249b77
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-02-18 02:02:50 +0400

    feat: add button for making new embeds

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
+import { useState } from "react";
 import Embed from "./embed";
 
 export default function Home() {
+  const [embeds, setEmbeds] = useState([] as number[]);
+
+  function addEmbed() {
+    setEmbeds([...embeds, 0]);
+  }
+
   return (
     <main className="container mx-auto py-10">
       <div className="flex justify-center my-5 p-2 rounded text-4xl font-bold">
@@ -12,8 +21,17 @@ export default function Home() {
         className="my-3 bg-transparent focus-visible:outline-none"
       />
       <div className="embeds">
-        <Embed></Embed>
+        {embeds.map((item, i) => (
+          <Embed key={i} />
+        ))}
       </div>
+
+      <button
+        onClick={addEmbed}
+        className="mt-2 w-fit h-7 px-2 bg-lime-700 rounded border border-lime-800 hover:bg-lime-600 hover:border-lime-500 transition"
+      >
+        +Embed
+      </button>
     </main>
   );
 }
