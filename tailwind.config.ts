7c6597a38c7f0857e2ff74e5ac6e939465a6fcb5
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-03-04 14:08:36 +0400

    fix: :ambulance: add custom tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,12 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        background: "#313339",
+        primary: "#248046",
+        primaryFocus: "#1A6334",
+        secondary: "#5765F2",
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
