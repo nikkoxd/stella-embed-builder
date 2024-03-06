5a250f70bbe7a97da65036bf175e0796815e625b
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-03-06 12:43:59 +0400

    feat: :lipstick: add new tw colors matching discord's

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "#313339",
+        bg: "#313339",
+        "bg-secondary": "#2B2D31",
         primary: "#248046",
-        primaryFocus: "#1A6334",
+        "primary-focus": "#1A6334",
         secondary: "#5765F2",
+        "secondary-focus": "#4752C4",
       },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
